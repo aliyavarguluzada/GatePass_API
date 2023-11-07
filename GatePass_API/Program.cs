09e37c954b9e6ce095b5d4465b7e5bccc5b3443c
aliyavar_guluzada <aliyavarguluzada@gmail.com>
2023-11-07 22:40:43 +0400

    Added JwtAuth but shoud use it in the controller

--- GatePass_API/Program.cs
+++ GatePass_API/Program.cs
@@ -1,10 +1,34 @@
 using GatePass_API.Data;
 using GatePass_API.Interfaces;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.IdentityModel.Tokens;
+using System.Text;
 
 var builder = WebApplication.CreateBuilder(args);
 
+var configuration = builder.Configuration;
 
+builder.Services.AddAuthentication(c =>
+{
+
+    c.DefaultAuthenticateScheme = JwtBearerDefaults.AuthenticationScheme;
+    c.DefaultChallengeScheme = JwtBearerDefaults.AuthenticationScheme;
+    c.DefaultScheme = JwtBearerDefaults.AuthenticationScheme;
+
+}).AddJwtBearer(c =>
+{
+    c.TokenValidationParameters = new TokenValidationParameters
+    {
+        ValidIssuer = configuration["JwtSettings:Issuer"],
+        IssuerSigningKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(configuration["JwtSettings:Key"])),
+        ValidateIssuer = true,
+        ValidateLifetime = true,
+        ValidateIssuerSigningKey = true
+    };
+});
+
+builder.Services.AddAuthorization();
 builder.Services.AddControllers();
 
 builder.Services.AddEndpointsApiExplorer();
@@ -16,6 +40,8 @@ builder.Services.AddTransient<IUserService, UserService>();
 builder.Services.AddDbContext<ApplicationDbContext>(
         options => options.UseSqlServer(builder.Configuration["Database:Connection"]));
 
+
+
 var app = builder.Build();
 
 if (app.Environment.IsDevelopment())
@@ -25,6 +51,8 @@ if (app.Environment.IsDevelopment())
 }
 
 app.UseHttpsRedirection();
+
+app.UseAuthentication();
 
 app.UseAuthorization();
 
