0618d11a6b4d52031d641a6b2ab9c47fbb787344
aliyavar_guluzada <aliyavarguluzada@gmail.com>
2023-11-07 22:13:53 +0400

    Changed method name AddUser To Register

--- GatePass_API/Controllers/AccountController.cs
+++ GatePass_API/Controllers/AccountController.cs
@@ -20,7 +20,7 @@ namespace GatePass_API.Controllers
 
 
         [HttpPost]
-        public async Task<ActionResult> AddUser(AddUserRequests request)
+        public async Task<ActionResult> Register(AddUserRequests request)
         {
             var user = new User
             {
